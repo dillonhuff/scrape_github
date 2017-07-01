3c56ace1aa53f7c3db471c55d57daff6b1fecf95
Dillon Huff <dillonhuff@gmail.com>
2017-07-01 15:52:31 -0700

    Read stars actually looks ok

--- scrape.php
+++ scrape.php
@@ -1,7 +1,7 @@
 <?php
 
 include('/Users/dillon/Downloads/simplehtmldom_1_5/simple_html_dom.php');
-$url = 'https://github.com/dillonhuff/gpr/blob/master/src/gcode_program.h';
+$url = 'https://github.com/dillonhuff/stl_parser';
 $output = file_get_contents($url);
 $html = str_get_html($output);
 
