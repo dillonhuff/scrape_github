ddc0009d3d682a6c9b3ea903dee57f07b5469510
Dillon Huff <dillonhuff@gmail.com>
2017-07-01 15:37:04 -0700

    Added htmldom

--- scrape.php
+++ scrape.php
@@ -1,5 +1,8 @@
 <?php
-$url = 'http://www.onshape.com';
+
+include('/Users/dillon/Downloads/simplehtmldom_1_5/simple_html_dom.php');
+$url = 'https://github.com/dillonhuff/gpr/blob/master/src/gcode_program.h';
 $output = file_get_contents($url);
+$html = str_get_html($output);
 echo $output;
 ?>
