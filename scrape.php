eb5a1866a83e8986504a73feed6d04b5aad6a451
Dillon Huff <dillonhuff@gmail.com>
2017-07-01 15:51:39 -0700

    Can now print out the number of stars on a page

--- scrape.php
+++ scrape.php
@@ -4,5 +4,13 @@ include('/Users/dillon/Downloads/simplehtmldom_1_5/simple_html_dom.php');
 $url = 'https://github.com/dillonhuff/gpr/blob/master/src/gcode_program.h';
 $output = file_get_contents($url);
 $html = str_get_html($output);
-echo $output;
+
+$ret = $html->find('a[class="social-count js-social-count"]');
+
+foreach($ret as $element)
+	     // echo count($element->children());
+       	     // echo $element;
+	     echo $element->innertext . "\n";
+
+//echo $output;
 ?>
